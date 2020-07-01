13e6e9533d489d8242a4f9a47bd75119173456e3
“nahuel” <n.gorosito@yahoo.com>
2020-07-01 19:56:42 -0400

    added various game over messages based on score ranges

--- src/index.js
+++ src/index.js
@@ -98,22 +98,18 @@ function main() {
       let data = new Uint8Array(analyser.frequencyBinCount);
       audioSrc.connect(analyser);
 
-      let test = document.createElement('aside');
-      let pic = document.createElement('img');
-      let pic2 = document.createElement('img');
-      let pic3 = document.createElement('img');
-      pic.style.width = 7 + '%';
-      pic.style.height = 7 + '%';
-      pic2.style.width = 7 + '%';
-      pic2.style.height = 7 + '%';
-      pic3.style.width = 7 + '%';
-      pic3.style.height = 7 + '%';
-      test.appendChild(pic);
-      test.appendChild(pic2);
-      test.appendChild(pic3);
-      test.style.animation = 'quick-scroll 15s linear infinite';
-      test.style.position = 'relative';
-      effects.insertBefore(test, effects.childNodes[0]);
+      const frame = document.createElement('aside');
+      const pic1 = document.createElement('img');
+      const pic2 = document.createElement('img');
+      const pic3 = document.createElement('img');
+      [pic1, pic2, pic3].forEach(pic => {
+        pic.setAttribute("style", "height: 7%; width: 7%;");
+        frame.appendChild(pic);
+      })
+
+      frame.style.animation = 'quick-scroll 15s linear infinite';
+      frame.style.position = 'relative';
+      effects.insertBefore(frame, effects.childNodes[0]);
       effects.style.height = 'fit-content';
 
       const score = document.createElement('p');
@@ -124,11 +120,12 @@ function main() {
        processor.onaudioprocess = function() {
           //  analyser.getByteTimeDomainData(data);
          analyser.getByteFrequencyData(data);
-         pic.src =  dblImages[Math.floor(Math.random(Array.from(Array(data[0]).keys())) * 100)].src;
+         pic1.src =  dblImages[Math.floor(Math.random(Array.from(Array(data[0]).keys())) * 100)].src;
          pic2.src = dblImages[Math.floor(Math.random(Array.from(Array(100).keys())) * 100)].src;
          pic3.src = dblImages[data[0]].src;
 
-         const slots = [pic.src, pic2.src, pic3.src];
+         let iconsLeft = effects.querySelectorAll('img').length;
+         const slots = [pic1.src, pic2.src, pic3.src];
          images.forEach(image => {
            if(slots.includes(image.src)) {
              image.style.animation = 'flicker 0.5s infinite alternate';
@@ -147,14 +144,14 @@ function main() {
               if (slots.includes(image.src)) {
                 effects.removeChild(image);
                 song.playbackRate -= 0.8;
-                console.log(effects.querySelectorAll('img').length);
+                console.log(iconsLeft);
               } else {
                 song.playbackRate += 0.1;
               }
-              score.innerHTML = 103 - effects.querySelectorAll('img').length;
+              score.innerHTML = 103 - iconsLeft;
             }
           })
-         if (effects.querySelectorAll('img').length <= 3) {
+         if ( iconsLeft <= 3) {
            song.playbackRate += 1;
            song.volume -= 0.7;
            setTimeout(function () { song.currentTime += 400; }, 4000);
@@ -162,31 +159,59 @@ function main() {
          
        } 
        song.onended = function() {
+         let iconsLeft = effects.querySelectorAll('img').length;       
          const end = new Date();
-         const diff = end - start;
-         console.log(end, diff);
-         const bonus = Math.round(((song.duration) - (diff / 1000)) * (100 - effects.querySelectorAll('img').length));
+         const diff = (end - start) / 1000;
+         let bonus; 
          const msg = document.createElement('h1');
-         msg.setAttribute("style", "font-size:60px; position: absolute; left: 150%; top: 30%; white-space: nowrap;");
-        //  msg.style.fontSize = 60 + 'px';
-        //  msg.style.position = 'absolute';
-        //  msg.style.left = 150 + '%';
-        //  msg.style.top = 30 + '%';
-        //  msg.style.whiteSpace = 'nowrap';
-        //  msg.style.fontFamily = 'Exo 2, sans-serif';
+         msg.id = "msg";
+         msg.setAttribute("style", "font-size:30px; position: absolute; left: 150%; top: 30%; white-space: nowrap; border: 2px solid currentColor; border-radius: 14px; padding: 14px; background-color: black; text-align: center;");
          tracks.appendChild(msg);
-        //  test.style.display = 'none';
-         effects.removeChild(test);
-         if (effects.querySelectorAll('img').length <= 50) {
-           msg.innerHTML = `You cleared: ${100 - effects.querySelectorAll('img').length} <br/> Bonus: ${bonus} <br/> YOU WIN!`;
-           msg.style.color = '#1ec190';
-           console.log('you win!', effects.querySelectorAll('img').length, bonus);
-         } else {
-           msg.innerHTML = `You cleared: ${100 - effects.querySelectorAll('img').length} <br/> Bonus: ${bonus} <br/> YOU LOSE!`;
-           msg.style.color = 'crimson';
-           console.log('you lose!', effects.querySelectorAll('img').length, bonus);
+         effects.removeChild(frame);
+         console.log(`icons left: ${iconsLeft}`);
+         
+         if (iconsLeft <= 15) {
+           bonus = Math.round((song.duration - diff) * (103 - iconsLeft));
+           msg.innerHTML = `You cleared <strong>${100 - iconsLeft}</strong> sounds off the board
+           like a true baw$$!!<br/> And as if that weren't enough, 
+           you got <strong>${bonus}</strong> bonus points for waving your mouse extra maniacally...<br/>Don't let it get to your head now.`;
+           msg.style.color = '#26dc08';
          }
-         setTimeout(function(){msg.style.display = 'none';}, 5000)
+         if (iconsLeft > 15 && iconsLeft <= 30) {
+           bonus = Math.round((song.duration / diff) * (103 - iconsLeft))
+           msg.innerHTML = `You cleared <strong>${103 - iconsLeft}</strong> sounds off the board like a maybe soon to be baw$$, not bad.<br/> 
+           you got <strong>${bonus}</strong> bonus points for efficiency.`;
+           msg.style.color = '#d4f575';
+         } 
+         if (iconsLeft > 30 && iconsLeft <= 50) {
+           bonus = Math.round((song.duration - diff));
+           msg.innerHTML = `You cleared <strong>${103 - iconsLeft}</strong> sound like a true average Joe, just enough to get by.<br/> 
+           You get <strong>${bonus}</strong> bonus points for doing you.`;
+           msg.style.color = '#e0e2d5';
+         }
+         if (iconsLeft > 50 && iconsLeft <= 75) {
+           bonus = Math.round(song.duration / diff);
+           msg.innerHTML = `Hmm, you only cleared <strong>${103 - iconsLeft}</strong> sounds...
+           You umm, got something on your mind?<br/> 
+           eh it could be worse, at least you got a little pocket change.<br/>  
+           Here, take these <strong>${bonus}</strong> bonus points and go buy yourself a soup or something.`;
+           msg.style.color = 'crimson';
+         }
+         if (iconsLeft > 75) {
+           bonus = Math.round(diff - song.duration - 1);
+           msg.innerHTML = `Whoa whoa whoa...are you a bot? Maybe you saw a mouse and
+           you to go deal with that during the song? <br/> If not, then it might be time for
+           some serious self-reflection and ask yourself, <br/> "how much longer do I want to be the
+           person who only clears <strong>${103 - iconsLeft}</strong> sounds off the board"? <br/>
+           Well here, take these  <strong>${bonus}</strong> bonus -- oh wait, no, it looks like you owe me points.`;
+           msg.style.left = 120 + '%';
+           msg.style.color = 'rgb(113, 8, 8)';
+           msg.style.textAlign = 'left';
+         }
+        //  setTimeout(function(){msg.style.display = 'none';}, 15000);
+        window.onclick = function() {
+          msg.style.display = 'none';
+        }
        }
     
     }
