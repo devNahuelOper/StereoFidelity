7a3ab04454da49820ff7d2d67231ecf7dc41500a
“nahuel” <n.gorosito@yahoo.com>
2020-10-26 10:27:55 -0400

    clear console error

--- src/index.js
+++ src/index.js
@@ -92,13 +92,13 @@ function main() {
   // (currbgIdx += i)
   // Math.abs((currbgIdx -= i))
 
-  function handleFiles(event) {
-    let files = event.target.files;
-    $("#src").attr("src", URL.createObjectURL(files[0]));
-    document.getElementById("audio").load();
-  }
+  // function handleFiles(event) {
+  //   let files = event.target.files;
+  //   $("#src").attr("src", URL.createObjectURL(files[0]));
+  //   document.getElementById("audio").load();
+  // }
 
-  document.getElementById("upload").addEventListener("change", handleFiles, false);
+  // document.getElementById("upload").addEventListener("change", handleFiles, false);
 
 
 
