bcad7d4a9fbca4c97a8ed410607a36765cb9ab87
“nahuel” <n.gorosito@yahoo.com>
2020-11-18 12:35:26 -0500

    Make separate script for background toggle functions

--- src/index.js
+++ src/index.js
@@ -3,74 +3,20 @@ import "./styles/effects.scss";
 import "./styles/mediaQueries.scss";
 import "./styles/inputSlider.scss";
 import { toggleModal, toggleSecretPlaylist } from "./scripts/modal";
+import toggleBackgrounds from "./scripts/backgrounds";
 
 window.addEventListener("DOMContentLoaded", main);
 
 function main() {
+  
   console.log(performance.now());
-
   const stage = document.querySelector("main");
 
   toggleModal();
   toggleSecretPlaylist();
 
-  const backgrounds = [
-    "src/assets/images/headphones.jpg",
-    "src/assets/images/face.jpg",
-    "src/assets/images/waves.jpg",
-    "src/assets/images/deadmau5.jpg",
-    "src/assets/images/buttons.jpg",
-    "src/assets/images/turntable.png",
-    "src/assets/images/mixing.jpg",
-  ];
+  toggleBackgrounds();
 
-  function randomBg() {
-    let num = Math.floor(Math.random(backgrounds) * backgrounds.length);
-    document.body.style.backgroundImage = "url('" + backgrounds[num] + "')";
-  }
-
-  const bgtoggle = document.getElementById("bgtoggle");
-  const bgcontainer = document.getElementsByClassName("bgtoggle-container")[0];
-
-  bgtoggle.onclick = function () {
-    randomBg();
-  };
-
-  function showBackgrounds() {
-    let ul = document.createElement("ul");
-    ul.className = "bg-list";
-
-    for (let bg of backgrounds) {
-      let li = document.createElement("li");
-      let img = document.createElement("img");
-      img.src = bg;
-      li.append(img);
-      ul.append(li);
-
-      img.onclick = function () {
-        document.body.style.backgroundImage = "url('" + bg + "')";
-      };
-    }
-    return ul;
-  }
-
-  let bglist;
-  bgtoggle.onpointerover = function () {
-    if (bglist) return;
-    bglist = showBackgrounds();
-    bgcontainer.append(bglist);
-    bglist.scrollIntoView();
-  };
-
-  bgcontainer.onpointerout = function (event) {
-    let target = event.relatedTarget;
-    let inContainer = bgcontainer.contains(target);
-
-    if (bglist && !inContainer) {
-      bglist.remove();
-      bglist = null;
-    }
-  };
 
   const effects = document.getElementsByClassName("effects")[0];
   const sounds = effects.querySelectorAll("audio");
---
+++ src/scripts/backgrounds.js
@@ -0,0 +1,61 @@
+function toggleBackgrounds() {
+  const backgrounds = [
+    "src/assets/images/headphones.jpg",
+    "src/assets/images/face.jpg",
+    "src/assets/images/waves.jpg",
+    "src/assets/images/deadmau5.jpg",
+    "src/assets/images/buttons.jpg",
+    "src/assets/images/turntable.png",
+    "src/assets/images/mixing.jpg",
+  ];
+
+  function randomBg() {
+    let num = Math.floor(Math.random(backgrounds) * backgrounds.length);
+    document.body.style.backgroundImage = "url('" + backgrounds[num] + "')";
+  }
+
+  const bgtoggle = document.getElementById("bgtoggle");
+  const bgcontainer = document.getElementsByClassName("bgtoggle-container")[0];
+
+  bgtoggle.onclick = function () {
+    randomBg();
+  };
+
+  function showBackgrounds() {
+    let ul = document.createElement("ul");
+    ul.className = "bg-list";
+
+    for (let bg of backgrounds) {
+      let li = document.createElement("li");
+      let img = document.createElement("img");
+      img.src = bg;
+      li.append(img);
+      ul.append(li);
+
+      img.onclick = function () {
+        document.body.style.backgroundImage = "url('" + bg + "')";
+      };
+    }
+    return ul;
+  }
+
+  let bglist;
+  bgtoggle.onpointerover = function () {
+    if (bglist) return;
+    bglist = showBackgrounds();
+    bgcontainer.append(bglist);
+    bglist.scrollIntoView();
+  };
+
+  bgcontainer.onpointerout = function (event) {
+    let target = event.relatedTarget;
+    let inContainer = bgcontainer.contains(target);
+
+    if (bglist && !inContainer) {
+      bglist.remove();
+      bglist = null;
+    }
+  };
+}
+
+export default toggleBackgrounds;
