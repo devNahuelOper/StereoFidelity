2da9783ce615332f34332de18e0610720df32b51
“nahuel” <n.gorosito@yahoo.com>
2020-12-26 00:02:43 -0500

    Save webcam access function

--- src/scripts/util.js
+++ src/scripts/util.js
@@ -1,3 +1,15 @@
 export const setAttributes = (ele, attrs) => {
   for (let key in attrs) ele.setAttribute(key, attrs[key]);
 }
+
+// let constraints = { audio: true, video: { width: 1280, height: 720 } };
+
+// navigator.mediaDevices
+//   .getUserMedia(constraints)
+//   .then((mediaStream) => {
+//     let video = document.createElement(video);
+//     document.body.append(video);
+//     video.srcObject = mediaStream;
+//     video.onloadedmetadata = () => video.play();
+//   })
+//   .catch((err) => console.log(err));
